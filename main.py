629228b06e2a5e82b34432d4ad538f6cfbb3388b
Varun Agarwal <165192602+Agarwalvarun23@users.noreply.github.com>
2025-06-28 00:02:32 +0530

    rules and choices

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ print("Rules of Game:\n1. Rock vs Paper -> Paper wins\n2. Rock vs Scissor -> Roc
 while True:
     print("Press 1 for Rock\nPress 2 for Paper\nPress 3 for Scissor\nPress 4 to Exit")
     choice = int(input("Enter your choice: "))
-    while choice >3 or choice<1:
-        print("Enter a valid choice:")
+    while choice>4 or choice<1:
+        choice = int(input("Enter your choice again:"))
     if choice == 1:
-        c_name = rock
+        c_name = "rock"
     elif choice == 2:
-        c_name = paper
+        c_name = "paper"
+    elif choice == 4:
+        break    
     else:
-        c_name = scissor
+        c_name = "scissor"  
     print(f"Your choice is {c_name}")                
