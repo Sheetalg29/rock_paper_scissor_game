954adb19ad633f45a9c727311c93560e398da973
Varun Agarwal <165192602+Agarwalvarun23@users.noreply.github.com>
2025-06-27 23:47:11 +0530

    rules

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 print("****ROCK PAPER SCISSOR GAME***")
+print("Rules of Game:\n1. Rock vs Paper -> Paper wins\n2. Rock vs Scissor -> Rock wins\n3. Paper vs Scissor -> Scissor wins")
+#while True:
+    
 print("Press 1 for Rock\nPress 2 for Paper\nPress 3 for Scissor\nPress 4 to Exit")
+
