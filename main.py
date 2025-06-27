cbde1d4ffbe46d1ed7ba837077c38f39f00d44be
Varun Agarwal <165192602+Agarwalvarun23@users.noreply.github.com>
2025-06-27 23:54:44 +0530

    choice

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 print("****ROCK PAPER SCISSOR GAME***")
 print("Rules of Game:\n1. Rock vs Paper -> Paper wins\n2. Rock vs Scissor -> Rock wins\n3. Paper vs Scissor -> Scissor wins")
-#while True:
-    
-print("Press 1 for Rock\nPress 2 for Paper\nPress 3 for Scissor\nPress 4 to Exit")
-
+while True:
+    print("Press 1 for Rock\nPress 2 for Paper\nPress 3 for Scissor\nPress 4 to Exit")
+    choice = int(input("Enter your choice: "))
+    while choice >3 or choice<1:
+        print("Enter a valid choice:")
+    if choice == 1:
+        c_name = rock
+    elif choice == 2:
+        c_name = paper
+    else:
+        c_name = scissor
+    print(f"Your choice is {c_name}")                
